7a0599c72e6f43b98cb7920a42a0906b8419fbfe
Gilles <gilles.gelan@izile.fr>
2018-01-08 15:26:14 -0400

    Modification police, affichage mail de commande

--- catalog/controller/mail/order.php
+++ catalog/controller/mail/order.php
@@ -77,6 +77,9 @@ class ControllerMailOrder extends Controller {
 		$data['text_telephone'] = $language->get('text_telephone');
 		$data['text_ip'] = $language->get('text_ip');
 		$data['text_order_status'] = $language->get('text_order_status');
+		$data['text_order_status2'] = $language->get('text_order_status2');
+		$data['text_order_status3'] = $language->get('text_order_status3');
+		$data['text_order_status4'] = $language->get('text_order_status4');
 		$data['text_payment_address'] = $language->get('text_payment_address');
 		$data['text_shipping_address'] = $language->get('text_shipping_address');
 		$data['text_product'] = $language->get('text_product');
@@ -284,6 +287,9 @@ class ControllerMailOrder extends Controller {
 		$data['text_order_id'] = $language->get('text_order_id');
 		$data['text_date_added'] = $language->get('text_date_added');
 		$data['text_order_status'] = $language->get('text_order_status');
+		$data['text_order_status2'] = $language->get('text_order_status2');
+		$data['text_order_status3'] = $language->get('text_order_status3');
+		$data['text_order_status4'] = $language->get('text_order_status4');
 		$data['text_link'] = $language->get('text_link');
 		$data['text_comment'] = $language->get('text_comment');
 		$data['text_footer'] = $language->get('text_footer');
@@ -367,6 +373,9 @@ class ControllerMailOrder extends Controller {
 			$data['text_order_id'] = $this->language->get('text_order_id');
 			$data['text_date_added'] = $this->language->get('text_date_added');
 			$data['text_order_status'] = $this->language->get('text_order_status');
+			$data['text_order_status2'] = $this->language->get('text_order_status2');
+			$data['text_order_status3'] = $this->language->get('text_order_status3');
+			$data['text_order_status4'] = $this->language->get('text_order_status4');
 			$data['text_product'] = $this->language->get('text_product');
 			$data['text_total'] = $this->language->get('text_total');
 			$data['text_comment'] = $this->language->get('text_comment');
--- catalog/language/fr-Fr/mail/order_edit.php
+++ catalog/language/fr-Fr/mail/order_edit.php
@@ -2,9 +2,11 @@
 // Text
 
 $_['text_subject']      = '%s - Ordonnance Mise a jour %s';
-$_['text_order_id']     = 'Num&eacutero de commande';
-$_['text_date_added']   = 'Date ajout&eacutee:';
-$_['text_order_status'] = 'Votre commande a &eacutet&eacute mise &aacute jour pour le statut suivant:';
-$_['text_comment']      = 'Les commentaires pour votre commande sont les suivants:';
-$_['text_link']         = 'Pour afficher votre commande, cliquez sur le lien ci-dessous:';
-$_['text_footer']       = 'Veuillez r&eacutepondre &aacute cet e-mail si vous avez des questions.';
+$_['text_order_id']     = '&#8470';
+$_['text_date_added']   = '';
+$_['text_order_status'] = 'L&#39&eacutequipe de Migan vous informe que votre commande n&deg';
+$_['text_order_status2'] = 'effectu&eacutee le ';
+$_['text_order_status3'] = '&agrave changer de statut. Son nouveau statut est :' ;
+$_['text_comment']      = 'Les commentaires concernant votre commande sont les suivants:';
+$_['text_link']         = 'Pour consulter votre commande, veuillez cliquer sur le lien ci-dessous:';
+$_['text_footer']       = 'Si vous avez une quelconque question, veuillez r&eacutepondre &agrave cet e-mail. L&#39&eacutequipe de Migan vous rendra r&eacuteponse dans les plus brefs d&eacutelais.';
