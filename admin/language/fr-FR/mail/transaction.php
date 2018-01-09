38af864b119e5c9274edef30e7fa13932aa7b680
Gilles <gilles.gelan@izile.fr>
2018-01-09 11:18:38 -0400

    Modification mail notification d'avoir

--- admin/language/fr-FR/mail/transaction.php
+++ admin/language/fr-FR/mail/transaction.php
@@ -1,6 +1,6 @@
 <?php
 // Text
 $_['text_subject']  = '%s - Affiliate Credit';
-$_['text_received'] = 'You have received %s credit!';
-$_['text_total']    = 'Your total amount of credit is now %s.';
-$_['text_credit']   = 'Your account credit can deducted from your next purchase.';
+$_['text_received'] = 'F&eacutelicitations ,vous avez re&ccedilu %s de cr&eacutedit!';
+$_['text_total']    = 'Le montant total de votre cr&eacutedit est maintenant de %s.';
+$_['text_credit']   = 'Ce montant pourra &ecirctre d&eacuteduit de votre prochaine commande. L&acute&eacutequipe Migan vous remercie pour la confiance accord&eacutee et esp&egravere vous revoir tr&egraves prochainement.';
